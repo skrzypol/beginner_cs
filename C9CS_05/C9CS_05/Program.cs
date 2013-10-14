2c7bc5c24a310bad71fafe02fd8cd11c1a71270f
Krzysztof S. <kskrzypinski@gmail.com>
2013-10-14 22:41:19 +0200

    After 5 lessons with Bob

---
+++ C9CS_05/C9CS_05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9CS_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //int y;
+
+            //x = 7;
+            //y = x + 3;
+            //Console.WriteLine(y);
+
+
+            //string myFirstName;
+            //myFirstName = "Kris";
+
+            //string myFirstName = "Kris";
+
+            //var myFirstName = "Kris";
+
+            //Console.WriteLine(myFirstName);
+
+            int x = 7;
+            string y = "5";
+            string myFirstTry = x.ToString() + y;
+            int mySecondTry = x + int.Parse(y);
+
+            Console.WriteLine(myFirstTry);
+            Console.WriteLine(mySecondTry);
+
+            Console.ReadLine();
+
+        }
+    }
+}
